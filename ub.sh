6b6e96c3a453ff08821937885e2db0ee05734995
chiko911 <71180624+chiko911@users.noreply.github.com>
2021-03-24 05:15:50 +0200

    Update ub.sh

--- ub.sh
+++ ub.sh
@@ -3,10 +3,8 @@ sudo apt install xfce4 xfce4-goodies
 sudo apt install tightvncserver
 sudo apt install xfonts-base
 sudo apt install autocutsel
-
 mv /root/.vnc/xstartup ~/.vnc/xstartup.bak
 cd /root/.vnc/
-
 cat <<EOF >>xstartup
 #!/bin/bash
 xrdb $HOME/.Xresources
@@ -14,7 +12,6 @@ startxfce4 &
 autocutsel -fork
 preved
 EOF
-
 sudo apt install firefox
 sudo chmod +x ~/.vnc/xstartup
 vncserver
